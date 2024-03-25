40c176e96259adf5d68a0be2d01ce739d68a8c25
itsaquestion <itsaquestion@gmail.com>
2024-03-25 17:15:57 +0800

    fix sync.sh

--- sync.sh
+++ sync.sh
@@ -1,5 +1,5 @@
 #/usr/bin/bash
-rsync -av -e ssh _book/ lee@cc.imtass.me:/www/py4ss.net/abm_reading 
+rsync -av -e ssh _site/ lee@cc.imtass.me:/www/py4ss.net/abm_reading 
 
 # # turn off cache = development_mode on 
 # curl -X PATCH "https://api.cloudflare.com/client/v4/zones/7e61015d60a07937b261e3314862c7a4/settings/development_mode" \
